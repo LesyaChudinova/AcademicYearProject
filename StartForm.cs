12d7c3ef3050e10e273a14a113e466f1ed04fc90
LesyaChudinova <hi.lesya.4567@gmail.com>
2025-05-19 20:45:03 +0500

    Исправление ошибок в проработанном дизайне

--- StartForm.cs
+++ StartForm.cs
@@ -20,6 +20,7 @@ namespace AcademicYearProject
 
         public StartScreen()
         {
+            InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
